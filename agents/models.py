1253dbfad3a380eb562c8f27043308ea538c19d3
ANDRIA-NTOANINA Herimiaina <herimiaina@yahoo.ca>
2018-10-16 02:27:59 +0300

    Started the implementation of a parallel_ddpg with noise applied to the parameter

---
+++ agents/models.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Actor(nn.Module):
+    def __init__(self, state_size, action_size, seed):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of action space
+            seed (int): Random seed
+        """
+        super().__init__()
+        self.seed = torch.manual_seed(seed)
+        hidden_layers_size = [state_size, 128, 64, 32, action_size]
+        self.hidden_layers = nn.ModuleList()
+        self.hidden_layers.extend([nn.Linear(hidden_layers_size[i], hidden_layers_size[i + 1]) for i in range(len(hidden_layers_size) - 1)])
+        self.normalization_layers = nn.ModuleList()
+        self.normalization_layers.extend([nn.BatchNorm1d(hidden_layers_size[i]) for i in range(1,len(hidden_layers_size))])
+    def forward(self, state):
+        """Build a network that maps state to actions."""
+        x = state
+        for i in range(len(self.hidden_layers)-1):
+            linear = self.hidden_layers[i]
+            batch_norm = self.normalization_layers[i]
+            x = batch_norm(F.relu(linear(x)))
+        last = self.hidden_layers[-1]
+        x = F.tanh(last(x))
+        return x
+
+class Critic(nn.Module):
+    def __init__(self, state_size, action_size, seed):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of action space
+            seed (int): Random seed
+        """
+        super().__init__()
+        self.seed = torch.manual_seed(seed)
+        hidden_layers_size = [state_size + action_size, 128, 64, 32, 1]
+        self.hidden_layers = nn.ModuleList()
+        self.hidden_layers.extend([nn.Linear(hidden_layers_size[i], hidden_layers_size[i + 1]) for i in range(len(hidden_layers_size) - 1)])
+        self.normalization_layers = nn.ModuleList()
+        self.normalization_layers.extend([nn.BatchNorm1d(hidden_layers_size[i]) for i in range(1,len(hidden_layers_size))])
+    def forward(self, state, action):
+        """Build a network that maps state to actions."""
+        x = torch.cat((state, action), dim=1)
+        for i in range(1, len(self.hidden_layers)-1):
+            linear = self.hidden_layers[i]
+            batch_norm = self.normalization_layers[i]
+            x = batch_norm(F.relu(linear(x)))
+        last = self.hidden_layers[-1]
+        x = last(x)
+        return x
---
+++ agents/parallel_ddpg.py
@@ -0,0 +1,172 @@
+import numpy as np
+
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+from collections import deque
+import random
+from agents.utils import soft_update, apply_noise, build_noise
+
+device = "cpu"
+class ParallelDDPG():
+    """Interacts with and learns from the environment."""
+
+    def __init__(self, state_size, action_size, seed, num_agents, memory, ActorNetwork, CriticNetwork, device,
+                BATCH_SIZE = 32,
+                GAMMA = 0.99, 
+                TAU = 0.5, 
+                LR_CRITIC = 5e-4,
+                LR_ACTOR = 5e-4, 
+                UPDATE_EVERY = 1,
+                TRANSFER_EVERY = 4,
+                FILE_NAME="parallel_dpg"):
+        """Initialize an Agent object.
+        
+        Params
+        ======
+            state_size (int): dimension of each state
+            action_size (int): dimension of each action
+            seed (int): random seed
+            num_agents: number of running agents
+            memory: instance of ReplayBuffer
+            ActorNetwork: a class inheriting from torch.nn.Module that define the structure of the actor neural network
+            CriticNetwork: a class inheriting from torch.nn.Module that define the structure of the critic neural network
+            device: cpu or cuda:0 if available
+            BATCH_SIZE: minibatch size
+            GAMMA: discount factor
+            TAU: for soft update of target parameters
+            LR_CRITIC: learning rate of the critics
+            LR_ACTOR: learning rate of the actors
+            UPDATE_EVERY: how often to update the networks
+            TRANSFER_EVERY: how often to transfer from the online network to the targeted fixed network
+            FILE_NAME: default prefix to the saved model
+        """
+        self.seed = random.seed(seed)
+        self.num_agents = num_agents
+        
+        # Actor networks
+        self.actors_local = [ActorNetwork(state_size, action_size, seed).to(device) for i in range(num_agents)]
+        self.actors_optim = [optim.Adam(self.actors_local[i].parameters(), lr=LR_ACTOR) for i in range(num_agents)]
+        self.actor_target = ActorNetwork(state_size, action_size, seed).to(device)
+                 
+        # Critic networks
+        self.critics_local = [CriticNetwork(state_size, action_size, seed).to(device) for i in range(num_agents)]
+        self.critics_optim = [optim.Adam(self.critics_local[i].parameters(), lr=LR_CRITIC) for i in range(num_agents)]
+        self.critic_target = CriticNetwork(state_size, action_size, seed).to(device)
+        
+        self.device = device
+        # Noise
+        
+        self.noise = build_noise(self.actor_target, device, seed)
+        
+        # Replay memory
+        self.memory = memory
+        # Initialize time steps (for updating every UPDATE_EVERY steps)
+        self.u_step = 0
+        self.t_step = 0
+        
+        self.BATCH_SIZE = BATCH_SIZE
+        self.GAMMA = GAMMA
+        self.TAU = TAU
+        self.LR_CRITIC = LR_CRITIC
+        self.LR_ACTOR = LR_ACTOR
+        self.UPDATE_EVERY = UPDATE_EVERY
+        self.TRANSFER_EVERY =TRANSFER_EVERY
+     
+    def save(self):
+        for i in range(self.num_agents):
+            torch.save(agent.critics_local[i].state_dict(),"{}_critic_{}.pth".format(self.FILE_NAME, i))
+            torch.save(agent.actors_local[i].state_dict(),"{}_actor_{}.pth".format(self.FILE_NAME, i))
+            torch.save(agent.critic_target.state_dict(),"{}_critic_{}.pth".format(self.FILE_NAME, "target"))
+            torch.save(agent.actor_target.state_dict(),"{}_actor_{}.pth".format(self.FILE_NAME, "target"))
+     
+    def load(self):
+        for i in range(self.num_agents):
+            agent.critics_local[i].load_state_dict(torch.load("{}_critic_{}.pth".format(self.FILE_NAME, i)))
+            agent.actors_local[i].load_state_dict(torch.load("{}_actor_{}.pth".format(self.FILE_NAME, i)))
+            agent.critic_target.load_state_dict(torch.load("{}_critic_{}.pth".format(self.FILE_NAME, "target")))
+            agent.actor_target.load_state_dict(torch.load("{}_actor_{}.pth".format(self.FILE_NAME, "target")))
+    
+    def reset(self):
+        self.noise.reset()
+
+    def act(self, states, add_noise=True):
+        """Returns actions of each actor for given states.
+        
+        Params
+        ======
+            state (array_like): current states
+        """
+        ret = np.array([])
+        
+        with torch.no_grad():
+            if add_noise:
+                noise_sample = self.noise.sample()
+                for i in range(self.num_agents):
+                    state = torch.from_numpy(states[i]).float().unsqueeze(0).to(device)
+                    actor_local = self.actors_local[i]
+                    actor_local.eval()
+                    # apply a noise to the parameters only for exploration purpose
+                    apply_noise(actor_local, noise_sample)
+                    to_add = actor_local(state).cpu().data.numpy().squeeze()
+                    ret = np.concatenate((to_add, ret))
+                    # restore the previous parameters otherwise the noise will disturb the acquired knowldege
+                    apply_noise(actor_local, -noise_sample)
+                    actor_local.train()
+            else:
+                for i in range(self.num_agents):
+                    state = torch.from_numpy(states[i]).float().unsqueeze(0).to(device)
+                    actor_local = self.actors_local[i]
+                    actor_local.eval()
+                    ret = np.concatenate(self.actor_local(state).cpu().data.numpy(), ret)
+                    actor_local.train()
+                
+        return np.flip(ret, axis=0)
+    
+    def step(self, states, actions, rewards, next_states, dones):
+        # Save experience in replay memory
+        for i in range(self.num_agents):
+            self.memory.add(states[i], actions[i], rewards[i], next_states[i], dones[i])
+        
+        # Learn every UPDATE_EVERY time steps.
+        self.t_step = (self.t_step + 1) % self.TRANSFER_EVERY
+        self.u_step = (self.u_step + 1) % self.UPDATE_EVERY
+        
+        if len(self.memory) > self.BATCH_SIZE and self.u_step == 0:
+            for i in range(self.num_agents):
+                experiences = self.memory.sample()
+                self.learn(experiences, self.actors_local[i], self.actors_optim[i], self.critics_local[i], self.critics_optim[i])
+        
+        if len(self.memory) > self.BATCH_SIZE and self.t_step == 0:
+            for i in range(self.num_agents):
+                soft_update(self.actors_local[i], self.critic_target[i], self.TAU)
+                soft_update(self.critics_local[i], self.critic_target[i], self.TAU)
+
+    def learn(self, experiences, actor_local, actor_optim, critic_local, critic_optim):
+        """Update value parameters using given batch of experience tuples.
+
+        Params
+        ======
+            experiences (Tuple[torch.Variable]): tuple of (s, a, r, s', done) tuples
+        """
+        states, actions, rewards, next_states, dones = experiences
+        
+        # The critic should estimate the value of the states to be equal to rewards plus
+        # the estimation of the next_states value according to the critic_target and actor_target
+        next_actions = self.actor_target(next_states)
+        targeted_value = rewards + self.GAMMA*self.critic_target(next_states, next_actions)
+        current_value = critic_local(states, actions)
+        
+        # calculate the loss and backprobagate
+        self.critic_optim.zero_grad()
+        F.mse_loss(current_value, targeted_value).backward()
+        self.critic_optim.step()
+        
+        # the actor's objective is to increase/decrease its parameters according to the value returned by the critic_local
+        # that is achieved by maximizing the return of the critic_local through a gradient ascent
+        actions_pred = actor_local(states)
+        self.actor_optim.zero_grad()
+        (- critic_local(states, actions_pred).mean()).backward()
+        self.actor_optim.step()
+
+    
