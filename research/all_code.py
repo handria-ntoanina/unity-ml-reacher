ec65f6d83a942d9c0524a420ba2a2e4c6cfd0466
ANDRIA-NTOANINA Herimiaina <herimiaina@yahoo.ca>
2018-10-25 17:45:40 +0300

    Copied from ShangTong and Cozmo25 Kept only necessary class and method under all.py Target: Compare and Find out why model_ppo and ppo.py do not converge

---
+++ research/all_code.py
@@ -0,0 +1,272 @@
+from collections import deque
+import torch
+import torch.tensor as tensor
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import argparse
+
+class Config:
+    DEVICE = torch.device('cpu')
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+        self.task_fn = None
+        self.optimizer_fn = None
+        self.actor_optimizer_fn = None
+        self.critic_optimizer_fn = None
+        self.network_fn = None
+        self.actor_network_fn = None
+        self.critic_network_fn = None
+        self.replay_fn = None
+        self.random_process_fn = None
+        self.discount = None
+        self.target_network_update_freq = None
+        self.exploration_steps = None
+        self.logger = None
+        self.history_length = None
+        self.double_q = False
+        self.tag = 'vanilla'
+        self.num_workers = 1
+        self.gradient_clip = None
+        self.entropy_weight = 0.01
+        self.use_gae = False
+        self.gae_tau = 1.0
+        self.target_network_mix = 0.001
+        self.min_memory_size = None
+        self.max_steps = 0
+        self.rollout_length = None
+        self.value_loss_weight = 1.0
+        self.iteration_log_interval = 30
+        self.categorical_v_min = None
+        self.categorical_v_max = None
+        self.categorical_n_atoms = 51
+        self.num_quantiles = None
+        self.optimization_epochs = 4
+        self.num_mini_batches = 32
+        self.termination_regularizer = 0
+        self.sgd_update_frequency = None
+        self.random_action_prob = None
+        self.__eval_env = None
+        self.log_interval = int(1e3)
+        self.save_interval = 0
+        self.eval_interval = 0
+        self.eval_episodes = 10
+        self.async_actor = True
+        self.state_dim = None
+        self.action_dim = None
+
+
+class BaseAgent:
+    def __init__(self, config):
+        self.config = config
+
+    def close(self):
+        close_obj(self.task)
+
+    def save(self, filename):
+        torch.save(self.network.state_dict(), filename)
+
+    def load(self, filename):
+        state_dict = torch.load(filename, map_location=lambda storage, loc: storage)
+        self.network.load_state_dict(state_dict)
+
+    def eval_step(self, state):
+        raise Exception('eval_step not implemented')
+
+    def eval_episode(self):
+        env = self.config.eval_env
+        state = env.reset()
+        total_rewards = 0
+        while True:
+            action = self.eval_step(state)
+            state, reward, done, _ = env.step(action)
+            total_rewards += reward
+            if done:
+                break
+        return total_rewards
+
+    def eval_episodes(self):
+        rewards = []
+        for ep in range(self.config.eval_episodes):
+            rewards.append(self.eval_episode())
+        self.config.logger.info('evaluation episode return: %f(%f)' % (
+            np.mean(rewards), np.std(rewards) / np.sqrt(len(rewards))))
+        
+class PPOAgent(BaseAgent):
+    def __init__(self, config):
+        BaseAgent.__init__(self, config)
+        self.config = config
+        self.task = config.task_fn()
+        self.network = config.network_fn()
+        self.opt = config.optimizer_fn(self.network.parameters())
+        self.total_steps = 0
+        self.online_rewards = np.zeros(config.num_workers)
+        self.episode_rewards = []
+        self.states = self.task.reset()
+        self.scores_list = []
+        self.scores_deque = deque(maxlen=100)
+
+    def step(self):
+        config = self.config
+        rollout = []
+        states = self.states
+        for _ in range(config.rollout_length):
+            actions, log_probs, _, values = self.network(states)
+            next_states, rewards, terminals, _ = self.task.step(actions.cpu().detach().numpy())
+            self.online_rewards += rewards
+            if np.any(terminals):
+                self.scores_deque.append(np.mean(self.online_rewards))
+                self.scores_list.append(np.mean(self.online_rewards))
+            for i, terminal in enumerate(terminals):
+                if terminals[i]:
+                    self.episode_rewards.append(self.online_rewards[i])
+                    self.online_rewards[i] = 0
+            rollout.append([states, values.detach(), actions.detach(), log_probs.detach(), rewards, 1 - terminals])
+            states = next_states
+            
+        self.states = states
+        pending_value = self.network(states)[-1]
+        rollout.append([states, pending_value, None, None, None, None])
+
+        processed_rollout = [None] * (len(rollout) - 1)
+        advantages = tensor(np.zeros((config.num_workers, 1)))
+        returns = pending_value.detach()
+        for i in reversed(range(len(rollout) - 1)):
+            states, value, actions, log_probs, rewards, terminals = rollout[i]
+            terminals = tensor(terminals).unsqueeze(1)
+            rewards = tensor(rewards).unsqueeze(1)
+            actions = tensor(actions)
+            states = tensor(states)
+            next_value = rollout[i + 1][1]
+            returns = rewards + config.discount * terminals * returns
+            if not config.use_gae:
+                advantages = returns - value.detach()
+            else:
+                td_error = rewards + config.discount * terminals * next_value.detach() - value.detach()
+                advantages = advantages * config.gae_tau * config.discount * terminals + td_error
+            processed_rollout[i] = [states, actions, log_probs, returns, advantages]
+
+        states, actions, log_probs_old, returns, advantages = map(lambda x: torch.cat(x, dim=0), zip(*processed_rollout))
+        advantages = (advantages - advantages.mean()) / advantages.std()
+        
+        batcher = Batcher(states.size(0) // config.num_mini_batches, [np.arange(states.size(0))])
+        for _ in range(config.optimization_epochs):
+            batcher.shuffle()
+            while not batcher.end():
+                batch_indices = batcher.next_batch()[0]
+                batch_indices = tensor(batch_indices).long()
+                sampled_states = states[batch_indices]
+                sampled_actions = actions[batch_indices]
+                sampled_log_probs_old = log_probs_old[batch_indices]
+                sampled_returns = returns[batch_indices]
+                sampled_advantages = advantages[batch_indices]
+
+                _, log_probs, entropy_loss, values = self.network(sampled_states, sampled_actions)
+                ratio = (log_probs - sampled_log_probs_old).exp()
+                obj = ratio * sampled_advantages
+                obj_clipped = ratio.clamp(1.0 - self.config.ppo_ratio_clip,
+                                          1.0 + self.config.ppo_ratio_clip) * sampled_advantages
+                policy_loss = -torch.min(obj, obj_clipped).mean(0) - config.entropy_weight * entropy_loss.mean()
+
+                value_loss = 0.5 * (sampled_returns - values).pow(2).mean()
+
+                self.opt.zero_grad()
+                (policy_loss + value_loss).backward()
+                nn.utils.clip_grad_norm_(self.network.parameters(), config.gradient_clip)
+                self.opt.step()
+
+        steps = config.rollout_length * config.num_workers
+        self.total_steps += steps
+
+class FCBody(nn.Module):
+    def __init__(self, state_dim, hidden_units=(128, 128), gate=F.relu):
+        super(FCBody, self).__init__()
+        dims = (state_dim, ) + hidden_units
+        self.layers = nn.ModuleList([layer_init(nn.Linear(dim_in, dim_out)) for dim_in, dim_out in zip(dims[:-1], dims[1:])])
+        self.gate = gate
+        self.feature_dim = dims[-1]
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = self.gate(layer(x))
+        return x
+
+class BaseNet:
+    def __init__(self):
+        pass
+
+class ActorCriticNet(nn.Module):
+    def __init__(self, state_dim, action_dim, actor_body, critic_body):
+        super(ActorCriticNet, self).__init__()
+        self.actor_body = actor_body
+        self.critic_body = critic_body
+        self.fc_action = layer_init(nn.Linear(actor_body.feature_dim, action_dim), 1e-3)
+        self.fc_critic = layer_init(nn.Linear(critic_body.feature_dim, 1), 1e-3)
+
+        self.actor_params = list(self.actor_body.parameters()) + list(self.fc_action.parameters())
+        self.critic_params = list(self.critic_body.parameters()) + list(self.fc_critic.parameters())
+        
+class GaussianActorCriticNet(nn.Module, BaseNet):
+    def __init__(self,
+                 state_dim,
+                 action_dim,
+                 phi_body=None,
+                 actor_body=None,
+                 critic_body=None):
+        super(GaussianActorCriticNet, self).__init__()
+        self.network = ActorCriticNet(state_dim, action_dim, actor_body, critic_body)
+        self.std = nn.Parameter(torch.ones(1, action_dim))
+        self.to(Config.DEVICE)
+
+    def forward(self, obs, action=None):
+        obs = tensor(obs)
+        phi_a = self.network.actor_body(obs)
+        phi_v = self.network.critic_body(obs)
+        mean = F.tanh(self.network.fc_action(phi_a))
+        v = self.network.fc_critic(phi_v)
+        dist = torch.distributions.Normal(mean, self.std)
+        if action is None:
+            action = dist.sample()
+        log_prob = dist.log_prob(action)
+        log_prob = torch.sum(log_prob, dim=1, keepdim=True)
+        return action, log_prob, tensor(np.zeros((log_prob.size(0), 1))), v
+
+class Batcher:
+    def __init__(self, batch_size, data):
+        self.batch_size = batch_size
+        self.data = data
+        self.num_entries = len(data[0])
+        self.reset()
+
+    def reset(self):
+        self.batch_start = 0
+        self.batch_end = self.batch_start + self.batch_size
+
+    def end(self):
+        return self.batch_start >= self.num_entries
+
+    def next_batch(self):
+        batch = []
+        for d in self.data:
+            batch.append(d[self.batch_start: self.batch_end])
+        self.batch_start = self.batch_end
+        self.batch_end = min(self.batch_start + self.batch_size, self.num_entries)
+        return batch
+
+    def shuffle(self):
+        indices = np.arange(self.num_entries)
+        np.random.shuffle(indices)
+        self.data = [d[indices] for d in self.data]
+
+    
+def tensor(x):
+    if isinstance(x, torch.Tensor):
+        return x
+    x = torch.tensor(x, device=Config.DEVICE, dtype=torch.float32)
+    return x
+
+def layer_init(layer, w_scale=1.0):
+    nn.init.orthogonal_(layer.weight.data)
+    layer.weight.data.mul_(w_scale)
+    nn.init.constant_(layer.bias.data, 0)
+    return layer
